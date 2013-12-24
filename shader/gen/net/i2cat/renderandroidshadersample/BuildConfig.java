81c50c8a70aa5aba2fe099058697e39ef2af87c9
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2013-12-24 10:46:45 +0100

    Remove old gen/ contents

--- shader/gen/net/i2cat/renderandroidshadersample/BuildConfig.java
+++ shader/gen/net/i2cat/renderandroidshadersample/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package net.i2cat.renderandroidsample;
+package net.i2cat.renderandroidshadersample;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- shader/gen/net/i2cat/renderandroidshadersample/R.java
+++ shader/gen/net/i2cat/renderandroidshadersample/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package net.i2cat.renderandroidsample;
+package net.i2cat.renderandroidshadersample;
 
 public final class R {
     public static final class attr {
